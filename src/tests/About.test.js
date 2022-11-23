ac565bb1a27a4d94ee5bcd8df099d8e6d537113e
viviannemelo <viviannemmelo@gmail.com>
2022-11-23 16:59:17 -0300

    Requisitos 1 e 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../pages/About';
+
+describe('2. Teste o componente <About.js />', () => {
+  test('2.1- Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const aboutHead = screen.getByRole('heading', {
+      name: /About Pokédex/i,
+      level: 2,
+    });
+    expect(aboutHead).toBeInTheDocument();
+  });
+
+  test('2.2- Teste se a página contém dois parágrafos com texto sobre Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const paragraphsPokedex = screen.getAllByText(/pokémon/i);
+    expect(paragraphsPokedex).toHaveLength(2);
+  });
+
+  test('2.3- Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const imgPokedex = screen.getByRole('img', { name: /pokédex/i });
+    const srcImgPokedex = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(imgPokedex).toBeInTheDocument();
+    expect(imgPokedex).toHaveProperty('src', srcImgPokedex);
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,78 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('1. Teste o componente <App.js />', () => {
+  test(
+    '1.1- Teste se o topo da aplicação contém um conjunto fixo de links de navegação',
+    () => {
+      renderWithRouter(<App />);
+
+      const linkHome = screen.getByRole('link', {
+        name: /Home/i,
+      });
+      expect(linkHome).toBeInTheDocument();
+
+      const linkAbout = screen.getByRole('link', {
+        name: /About/i,
+      });
+      expect(linkAbout).toBeInTheDocument();
+
+      const linkFavoritePokemon = screen.getByRole('link', {
+        name: /Favorite Pokémon/i,
+      });
+      expect(linkFavoritePokemon).toBeInTheDocument();
+    },
+  );
+
+  test(
+    '1.2- Teste se a aplicação é redirecionada para a página inicial Home',
+    () => {
+      const { history } = renderWithRouter(<App />);
+
+      const linkHome = screen.getByRole('link', {
+        name: /Home/i,
+      });
+
+      const { pathname } = history.location;
+      expect(pathname).toBe('/');
+
+      expect(linkHome).toBeInTheDocument();
+      userEvent.click(linkHome);
+    },
+  );
+
+  test(
+    '1.3- Teste se a aplicação é redirecionada para a página de About',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      const linkAbout = screen.getByRole('link', {
+        name: /About/i,
+      });
+
+      const { pathname } = history.location;
+      expect(pathname).toBe('/');
+
+      expect(linkAbout).toBeInTheDocument();
+      userEvent.click(linkAbout);
+    },
+  );
+
+  test(
+    '1.4- Teste se a aplicação é redirecionada para a página de Pokémons Favoritados',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      const linkFavoritePokemon = screen.getByRole('link', {
+        name: /Favorite Pokémon/i,
+      });
+
+      const { pathname } = history.location;
+      expect(pathname).toBe('/');
+
+      expect(linkFavoritePokemon).toBeInTheDocument();
+      userEvent.click(linkFavoritePokemon);
+    },
+  );
+});
