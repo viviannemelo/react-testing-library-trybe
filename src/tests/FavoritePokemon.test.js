bc589e3e16f1cca387c89144b38a506d06c2de8c
viviannemelo <viviannemmelo@gmail.com>
2022-11-23 17:02:10 -0300

    Requisito 03

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemon from '../pages/FavoritePokemon';
+
+describe('3. Teste o componente <FavoritePokemon.js >', () => {
+  test('3.1- Teste se é exibida na tela a mensagem "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemon />);
+    const favNotFound = screen.getByText('No favorite Pokémon found');
+    expect(favNotFound).toBeInTheDocument();
+  });
+});
